f892f51d676e7fa5cbacdd0edd3ac3616b9d5cf0
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-05-23 15:32:12 -0300

    part3-01
    part3-01

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,18 @@
 import { request, Router } from "express";
+import { UsersController } from "./controllers/UsersController";
 import { SettingsController } from "./controllers/SettingsController";
+import { MessagesController } from "./controllers/MessagesController";
 
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
+routes.post("/messages", messagesController.create);
+
 
 export { routes };
