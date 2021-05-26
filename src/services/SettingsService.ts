4bc78e641614ece3bdfc51217a93824aedc8af4e
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-05-26 11:00:56 -0300

    part4-02
    part4-02

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,33 +1,56 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 
 interface ISettingsCreate {
-    chat: boolean;
-    username: string;
+  chat: boolean;
+  username: string;
 }
 
 class SettingsService {
-    
-    async create({ chat, username }) {
-        const settingsRepository = getCustomRepository(SettingsRepository);
+  private settingsRepository: Repository<Setting>;
 
-        const userAlreadyExists = await settingsRepository.findOne({
-            username,
-        });
+  constructor() {
+    this.settingsRepository = getCustomRepository(SettingsRepository);
+  }
 
-        if (userAlreadyExists) {
-            throw new Error("User already exists!");
-        }
+  async create({ chat, username }: ISettingsCreate) {
+    //Select * from settings where username = "username" limit 1;
+    const userAlreadyExists = await this.settingsRepository.findOne({
+      username,
+    });
 
-        const settings = settingsRepository.create({
-            chat,
-            username,
-        });
-
-        await settingsRepository.save(settings);
-
-        return settings;
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
     }
+
+    const settings = this.settingsRepository.create({
+      chat,
+      username,
+    });
+
+    await this.settingsRepository.save(settings);
+
+    return settings;
+  }
+
+  async findByUsername(username: string) {
+    const settings = await this.settingsRepository.findOne({
+      username,
+    });
+    return settings;
+  }
+
+  async update(username: string, chat: boolean) {
+    await this.settingsRepository
+      .createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where("username = :username", {
+        username,
+      })
+      .execute();
+  }
 }
 
 export { SettingsService };
